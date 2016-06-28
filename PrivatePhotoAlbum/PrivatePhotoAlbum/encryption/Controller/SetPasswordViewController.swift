ab4c5a727d6da45fcf3b9e9bd6b5cded2793ca82
Dayuan <heidayuan@126.com>
2016-06-28 09:36:00 +0800

    啦啦

--- PrivatePhotoAlbum/PrivatePhotoAlbum/encryption/Controller/SetPasswordViewController.swift
+++ PrivatePhotoAlbum/PrivatePhotoAlbum/encryption/Controller/SetPasswordViewController.swift
@@ -8,22 +8,17 @@
 
 import UIKit
 class SetPasswordViewController: UIViewController {
-    let 密码 = UITextField()
-    let 重复密码 = UITextField()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let iconImageView:UIImageView = UIImageView(image: UIImage(named: "Icon"))
+        let iconImageView:UIImageView = UIImageView(image: UIImage(named: "Icon-60@3x"))
         iconImageView.translatesAutoresizingMaskIntoConstraints = false;
-        self.view.addSubview(iconImageView);
+        self.view.addSubview(iconImageView)
         self.view.addConstraint(NSLayoutConstraint.init(item: iconImageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0))
         self.view.addConstraint(NSLayoutConstraint.init(item: iconImageView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterY, multiplier: 0.3, constant: 0.0))
-        let 密码label = UILabel()
-        密码label.translatesAutoresizingMaskIntoConstraints = false;
-        密码label.text = "密码"
-        self.view.addSubview(密码label)
-        self.view.addConstraint(NSLayoutConstraint.init(item: 密码label, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: 密码label, attribute: NSLayoutAttribute.Left, multiplier: 10, constant: 0))
-        
+        self.view.addConstraint(NSLayoutConstraint.init(item: iconImageView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.Height, multiplier: 1.0, constant: 130));
+       
     
     
     }
